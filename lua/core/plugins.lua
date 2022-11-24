a0b76b7b3b31f122a975227ffaaa40c59c37f4b0
geovanni <geovani@exinnotech.com>
2022-11-24 10:53:10 -0600

    Añadiendo plugin necesarios para nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -3,6 +3,9 @@ local opts = {
   -- Gestor de paquetes
   { "wbthomason/packer.nvim" }, 
 
+  -- Dependencias necesarias
+  { "nvim-lua/plenary.nvim" },
+  { "tweekmonster/startuptime.vim", cmd = "StartupTime" },
 }
 
 return opts
