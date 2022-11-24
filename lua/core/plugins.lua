39f29c029e896f396a032a78c5ac66128bac5f07
geovanni <geovani@exinnotech.com>
2022-11-24 10:54:28 -0600

    Añadiendo tema para nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -6,6 +6,16 @@ local opts = {
   -- Dependencias necesarias
   { "nvim-lua/plenary.nvim" },
   { "tweekmonster/startuptime.vim", cmd = "StartupTime" },
+
+  -- Interfaz de Usuario
+  {
+    "Mofiqul/adwaita.nvim",
+    after = "plenary.nvim",
+    config = function()
+      vim.g.adwaita_darker = true
+      vim.cmd([[colorscheme adwaita]])
+    end
+  },
 }
 
 return opts
