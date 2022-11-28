0f32154ae781b8e0f3c73e9c0effefd4b3dfc8d8
geovanni <geovani@exinnotech.com>
2022-11-27 18:58:39 -0600

    Pequeño cambio para el módulo gitsigns

--- lua/plugins/gitsigns/init.lua
+++ lua/plugins/gitsigns/init.lua
@@ -1,4 +1,4 @@
-local ok_gitsigns, gitsigns = pcall(require, "plugins.gitsigns.config")
+local ok_config, config = pcall(require, "plugins.gitsigns.config")
 local ok_keybinds, keybinds = pcall(require, "plugins.gitsigns.keybinds")
-if not ok_gitsgins and ok_keybinds then return end
+if not ok_config and ok_keybinds then return end
 
