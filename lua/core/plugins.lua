fc16c6de8069b5a1ec9a057a8f7070336070f7af
geovanni <geovani@exinnotech.com>
2022-11-28 00:44:54 -0600

    Pequeño cambio para plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -40,7 +40,7 @@ local opts = {
   },
   {
     "lewis6991/gitsigns.nvim",
-    event = {"BufRead", "BufNewFile"},
+    event = { "BufRead", "BufNewFile" },
     config = function()
       require("plugins.gitsigns").start()
     end
