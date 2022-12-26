8305ba77a71b763e2647eecb8d897d0d69575b5e
geovanni <geovani@exinnotech.com>
2022-12-25 21:28:55 -0600

    Mínimos cambios para toggleterm

--- lua/plugins/toggleterm/init.lua
+++ lua/plugins/toggleterm/init.lua
@@ -9,28 +9,34 @@ M.opts = {
     end
   end,
   direction = "horizontal",
-  float_opts = {border = "curved"}
+  float_opts = { border = "curved" }
 }
 
 M.maps = {
-  { mode = {"n"}, lhs = "<leader>ts", rhs = "<cmd>ToggleTerm<cr>", options = {silent = true}, desc = "Abrir nueva terminal en el buffer actual" },
-  { mode = {"n"}, lhs = "<leader>tf", rhs = "<cmd>ToggleTerm direction=float<cr>", options = {silent = true}, desc = "Abrir nueva terminal en una ventana flotante" },
-  { mode = {"n"}, lhs = "<leader>tb", rhs = "<cmd>ToggleTerm direction=tab<cr>", options = {silent = true}, desc = "Abrir nueva terminal en una pestaña" },
-  { mode = {"n"}, lhs = "<leader>tt", rhs = "<cmd>ToggleTerm direction=vertical<cr>", options = {silent = true}, desc = "Iniciar Terminal verticalmente" },
-  { mode = {"t"}, lhs = "<esc>", rhs = "<C-\\><C-n>", options = {silent = true}, desc = "Escapa de la terminal" }
+  { mode = {"n"}, lhs = "<leader>tf", rhs = "<cmd>ToggleTerm direction=float<cr>", options = { silent = true }, desc = "Abrir nueva terminal en una ventana flotante" },
+  { mode = {"n"}, lhs = "<leader>tb", rhs = "<cmd>ToggleTerm direction=tab<cr>", options = { silent = true }, desc = "Abrir nueva terminal en una pestaña" },
+  { mode = {"n"}, lhs = "<leader>tt", rhs = "<cmd>ToggleTerm direction=vertical<cr>", options = { silent = true }, desc = "Iniciar Terminal verticalmente" },
+  { mode = {"n"}, lhs = "<leader>ts", rhs = "<cmd>ToggleTerm<cr>", options = { silent = true }, desc = "Abrir nueva terminal en el buffer actual" },
+  { mode = {"t"}, lhs = "<esc>", rhs = "<C-\\><C-n>", options = { silent = true }, desc = "Escapa de la terminal" }
 }
+
+M.terminal_keybinds = function()
+
+  local ok, keybinds = pcall(require,"core.functions")
+  if not ok then return end
+
+  return keybinds.load_keybinds(M.maps)
+
+end
 
 M.start = function()
 
   local ok, terminal = pcall(require, "toggleterm")
   if not ok then return end
-  
-  local ok_keybinds, keybinds = pcall(require,"core.functions")
-  if not ok_keybinds then return end
 
   terminal.setup(M.opts)
 
-  keybinds.load_keybinds(M.maps)
+  M.terminal_keybinds()
 
 end
 
