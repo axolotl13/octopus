cc579ee4cf26c3d16fc90c4bc7de17dbe729a2f9
geovanni <geovani@exinnotech.com>
2023-01-03 17:36:20 -0600

    Se añade módulo para el gestor de paquetes de instalación de servidores, formateo, linter, y debugger

---
+++ lua/plugins/_mason.lua
@@ -0,0 +1,47 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    { "williamboman/mason-lspconfig.nvim" }
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lsp = require("mason-lspconfig")
+    local icons = require("ui.icons")
+
+    local analizer = {
+      "prettierd",
+      "stylua",
+      "eslint_d",
+      "shfmt",
+      "black",
+      "isort",
+      "flake8",
+    }
+
+    local settings = {
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = icons.lsp.install,
+          package_pending = icons.lsp.pending,
+          package_uninstalled = icons.lsp.uninstall,
+        },
+        keymaps = {
+          install_package = "i",
+          update_package = "u",
+          uninstall_package = "d",
+          cancel_installation = "<C-c>",
+        }
+      },
+      log_level = vim.log.levels.INFO,
+      max_concurrent_installers = 4,
+    }
+
+    mason.setup(settings)
+    mason_lsp.setup({
+      automatic_installation = true
+    })
+
+  end,
+  keys = { { "<leader>m", "<cmd>Mason<cr>", desc = "Mason" } },
+}
