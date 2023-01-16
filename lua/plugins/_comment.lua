accce8ef72bec3b2115b8c5028092c816d83b524
geovanni <geovani@exinnotech.com>
2023-01-15 22:01:08 -0600

    Se hace pequeños cambios

--- lua/plugins/_comment.lua
+++ lua/plugins/_comment.lua
@@ -1,39 +1,30 @@
 return {
   "numToStr/Comment.nvim",
   -- event = "VeryLazy",
-  config = function()
-    local comment = require("Comment")
-
-    local opts = {
-      padding = true,
-      ignore = "^$",
-      mapping = {
-        basic = true,
-        extra = false
-      },
-      toggler = {
-        line = "gcc",
-        block = "gbc"
-      },
-      pre_hook = function(ctx)
-        local U = require "Comment.utils"
-
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
-
-        return require("ts_context_commentstring.internal").calculate_commentstring {
-          key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-          location = location,
-        }
+  opts = {
+    padding = true,
+    ignore = "^$",
+    mapping = {
+      basic = true,
+      extra = false
+    },
+    toggler = {
+      line = "gcc",
+      block = "gbc"
+    },
+    pre_hook = function(ctx)
+      local location = nil
+      if ctx.ctype == require("Comment.utils").ctype.block then
+        location = require("ts_context_commentstring.utils").get_cursor_location()
+      elseif ctx.cmotion == require("Comment.utils").cmotion.v or ctx.cmotion == require("Comment.utils").cmotion.V then
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
       end
-    }
 
-  comment.setup(opts)
-
-  end,
+      return require("ts_context_commentstring.internal").calculate_commentstring {
+        key = ctx.ctype == require("Comment.utils").ctype.line and "__default" or "__multiline",
+        location = location,
+      }
+    end
+  },
   keys = { "gcc", "gbc", { "gb", mode = "v" }, { "gc", mode = "v" } },
 }
