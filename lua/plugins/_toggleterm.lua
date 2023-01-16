703b21658bc3c77993c040986fdcb64ec9e275a4
geovanni <geovani@exinnotech.com>
2023-01-15 22:21:19 -0600

    Se hace pequeños cambios para toggleterm

--- lua/plugins/_toggleterm.lua
+++ lua/plugins/_toggleterm.lua
@@ -1,25 +1,18 @@
 return {
   "akinsho/toggleterm.nvim",
   cmd = "ToggleTerm",
-  config = function()
-    local toggleterm = require("toggleterm")
-
-    local opts = {
-      size = function(term)
-        if term.direction == "horizontal" then
-          return 14
-        elseif term.direction == "vertical" then
-          return vim.o.columns * 0.5
-        end
-      end,
-      direction = "horizontal",
-      close_on_exit = true,
-      float_opts = { border = "curved" },
-    }
-
-    toggleterm.setup(opts)
-
-  end,
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 16
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.5
+      end
+    end,
+    direction = "horizontal",
+    close_on_exit = true,
+    float_opts = { border = "curved" },
+  },
   keys = {
     { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "[Terminal] Abre nueva ventana" },
     { "<leader>tp", "<cmd>ToggleTerm direction=tab<cr>", desc = "[Terminal] Abre nueva pestaña" },
