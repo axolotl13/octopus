3e53a8bb95292600a5b0514d117f291210026ced
geovanni <geovani@exinnotech.com>
2023-01-15 18:19:19 -0600

    Se añadio descripciones para los atajos de teclado

--- lua/octopus/_maps.lua
+++ lua/octopus/_maps.lua
@@ -1,24 +1,23 @@
 local opts = {
   -- Designar atajos
-  { "", "<space>", "<nop>", "" },
-  { "n", "<c-z>", "<nop>", "" },
-  { "n", "<esc>", "<cmd>noh<cr>", "" },
-  { "n", "<esc>", "<cmd>noh<cr>", "" },
+  { "", "<space>", "<nop>", "Designar espacio" },
+  { "n", "<c-z>", "<nop>", "Designar ctrl + z" },
+  { "n", "<esc>", "<cmd>noh<cr>", "Designar esc" },
 
   -- Opciones para el cursor
-  { "n", "|", "^", "Mover el cursor al inicio" },
-  { "i", "<c-b>", "<ESC>^i", "Mover el cursor al inicio en modo insertar" },
-  { "i", "<c-e>", "<End>", "Mover el cursor al final en modo insertar" },
-  { "i", "<s-tab>", "<cmd>call search('[([{<]')<cr>", "Mover el cursor a los siguientes signos" },
+  { "n", "|", "^", "[Cursor] Mover al inicio" },
+  { "i", "<c-b>", "<ESC>^i", "[Cursor] Mover inicio en modo insertar" },
+  { "i", "<c-e>", "<End>", "[Cursor] Mover al final en modo insertar" },
+  --[[ { "i", "<s-tab>", "<cmd>call search('[([{<]')<cr>", "[Cursor] Mover a los siguientes signos" }, ]]
 
   -- Habilitar idioma
-  { "n", "<leader>es", "<cmd>set spell<cr>", "Habilita idioma en español" },
-  { "n", "<leader>nes", "<cmd>set nospell<cr>", "Deshabilita idioma en español" },
+  { "n", "<leader>es", "<cmd>set spell<cr>", "[Spell] Habilitar idioma en español" },
+  { "n", "<leader>nes", "<cmd>set nospell<cr>", "[Spell] Deshabilitar idioma en español" },
 
   -- Diferenciar archivos
-  { "n", "<a-e>", "<cmd>diffthis<cr>", "Habilita diff en el buffer actual" },
-  { "n", "<a-q>", "<cmd>diffoff<cr>", "Deshabilita diff en el buffer actual" },
-  { "n", "<a-w>", "<cmd>diffput<cr>", "Copia diff en el buffer" },
+  { "n", "<a-e>", "<cmd>diffthis<cr>", "[Diff] Habilitar diferencia en el buffer actual" },
+  { "n", "<a-q>", "<cmd>diffoff<cr>", "[Diff] Deshabilitar diferencia en el buffer actual" },
+  { "n", "<a-w>", "<cmd>diffput<cr>", "[Diff] Copia diferencia en el buffer" },
 
   -- Mover texto
   { "n", "<a-up>", ":m .-2<cr>==", "Mover línea hacía arriba" },
@@ -31,32 +30,32 @@ local opts = {
   { "v", ">", ">gv", "Aplica sangría hacía la derecha en modo visual" },
 
   -- Moverse entre ventanas
-  { "n", "<c-h>", "<c-w>h", "Moverse hacía la vista izquierda" },
-  { "n", "<c-l>", "<c-w>l", "Moverse hacía la vista derecha" },
-  { "n", "<c-k>", "<c-w>k", "Moverse hacía la vista de arriba" },
-  { "n", "<c-j>", "<c-w>j", "Moverse hacía la vista de abajo" },
+  { "n", "<c-h>", "<c-w>h", "[Window] Moverse hacía la vista izquierda" },
+  { "n", "<c-l>", "<c-w>l", "[Window] Moverse hacía la vista derecha" },
+  { "n", "<c-k>", "<c-w>k", "[Window] Moverse hacía la vista de arriba" },
+  { "n", "<c-j>", "<c-w>j", "[Window] Moverse hacía la vista de abajo" },
 
   -- Redimensionar ventana
-  { "n", "<c-up>", ":resize +2<cr>", "Redimensionar vista hacía arriba" },
-  { "n", "<c-down>", ":resize -2<cr>", "Redimensionar vista hacía abajo" },
-  { "n", "<c-left>", ":vertical resize -2<cr>", "Redimensionar vista hacía la izquierda" },
-  { "n", "<c-right>", ":vertical resize +2<cr>", "Redimensionar vista hacía la derecha" },
+  { "n", "<c-up>", ":resize +2<cr>", "[Window] Redimensionar vista hacía arriba" },
+  { "n", "<c-down>", ":resize -2<cr>", "[Window] Redimensionar vista hacía abajo" },
+  { "n", "<c-left>", ":vertical resize -2<cr>", "[Window] Redimensionar vista hacía la izquierda" },
+  { "n", "<c-right>", ":vertical resize +2<cr>", "[Window] Redimensionar vista hacía la derecha" },
 
   -- Navegación entre bufferes
-  { "n", "<tab>", "<cmd>bnext<cr>", "Moverse hacía el anterior buffer" },
-  { "n", "<s-tab>", "<cmd>bprevious<cr>", "Moverse hacía el siguiente buffer" },
-  { "n", "<c-s>", "<cmd>w!<cr>", "Guarda archivo actual" },
-  { "i", "<c-s>", "<cmd>w<cr><esc>", "Guarda archivo actual en modo insertar" },
-  { "n", "<leader>ba", "<cmd>%y+<cr>", "Copiar todo el contenido del buffer actual" },
-  { "n", "<leader>bq", "<cmd>bd!<pr>", "Cerrar buffer actual" },
-  { "n", "<leader>bn", "<cmd>enew<cr>", "Crear nuevo buffer" },
-  { "n", "<c-x>", "<cmd>qa!<cr>", "Cerrar todos los bufferes actuales" },
+  { "n", "<tab>", "<cmd>bnext<cr>", "[Buffer] Moverse hacía el anterior buffer" },
+  { "n", "<s-tab>", "<cmd>bprevious<cr>", "[Buffer] Moverse hacía el siguiente buffer" },
+  { "n", "<c-s>", "<cmd>w!<cr>", "[Save] Guarda archivo actual" },
+  { "i", "<c-s>", "<cmd>w<cr><esc>", "[Save] Guarda archivo actual en modo insertar" },
+  { "n", "<leader>ba", "<cmd>%y+<cr>", "[Buffer] Copiar todo el contenido del buffer actual" },
+  { "n", "<leader>bq", "<cmd>bd!<pr>", "[Buffer] Cerrar buffer actual" },
+  { "n", "<leader>bn", "<cmd>enew<cr>", "[Buffer] Crear nuevo buffer" },
+  { "n", "<c-x>", "<cmd>qa!<cr>", "[Buffer] Cerrar todos los bufferes actuales" },
 
   -- Navegación entre pestañas
-  { "n", "<leader>tn", "<cmd>tabnew<cr>", "Añadir nueva pestaña" },
-  { "n", "<leader>tj", "<cmd>tabprevious<cr>", "Moverse hacía la anterior pestaña" },
-  { "n", "<leader>tk", "<cmd>tabnext<cr>", "Moverse hacía la siguiente pestaña" },
-  { "n", "<leader>tq", "<cmd>tabclose<cr>", "Cerrar pestaña actual" },
+  { "n", "<leader>tn", "<cmd>tabnew<cr>", "[Pestaña] Añadir nueva pestaña" },
+  { "n", "<leader>tj", "<cmd>tabprevious<cr>", "[Pestaña] Moverse hacía la anterior pestaña" },
+  { "n", "<leader>tk", "<cmd>tabnext<cr>", "[Pestaña] Moverse hacía la siguiente pestaña" },
+  { "n", "<leader>tq", "<cmd>tabclose<cr>", "[Pestaña] Cerrar pestaña actual" },
 
   -- Terminal
   { "t", "<esc>", "<C-\\><C-n>", "Escapa de la terminal" },
@@ -70,6 +69,5 @@ local map = vim.keymap.set
 vim.g.mapleader = " "
 
 for _, key in pairs(opts) do
-  key.desc = key[4]
-  map(key[1], key[2], key[3], { silent = true })
+  map(key[1], key[2], key[3], { desc = key[4], silent = true })
 end
