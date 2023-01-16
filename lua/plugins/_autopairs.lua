008c5dd2b99bef6158921fba8b21fc6b7a7bab3b
geovanni <geovani@exinnotech.com>
2023-01-15 21:23:17 -0600

    Nueva devuelve a la aintigua forma

--- lua/plugins/_autopairs.lua
+++ lua/plugins/_autopairs.lua
@@ -1,28 +1,32 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {
-    check_ts = true,
-    ts_config = {
-      lua = { "string", "source" },
-      javascript = { "string", "template_string" }
-    },
-    disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
-    fast_wrap = {
-      map = "<M-e>", -- M = Alt
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-      offset = 0,
-      end_key = "$",
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      check_comma = true,
-      highlight = "PmenuSel",
-      highlight_grey = "LineNr"
-    }
-  },
   config = function()
-    require("nvim-autopairs").setup(opts)
+    local pairs = require("nvim-autopairs")
+
+    local opts = {
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" }
+      },
+      disable_filetype = { "TelescopePrompt", "vim" },
+      fast_wrap = {
+        map = "<M-e>", -- M = Alt
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr"
+      }
+    }
+    pairs.setup(opts)
+
     require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done(
       { map_char = { tex = "" } }))
+
   end
 }
