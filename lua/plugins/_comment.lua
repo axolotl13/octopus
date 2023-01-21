564f8356c088343b726f1ae7c3088ca3c6c9adfd
geovanni <geovani@exinnotech.com>
2023-01-21 16:00:41 -0600

    Se cambio a 'blockwise' en los comentarios

--- lua/plugins/_comment.lua
+++ lua/plugins/_comment.lua
@@ -14,14 +14,14 @@ return {
     },
     pre_hook = function(ctx)
       local location = nil
-      if ctx.ctype == require("Comment.utils").ctype.block then
+      if ctx.ctype == require("Comment.utils").ctype.blockwise then
         location = require("ts_context_commentstring.utils").get_cursor_location()
       elseif ctx.cmotion == require("Comment.utils").cmotion.v or ctx.cmotion == require("Comment.utils").cmotion.V then
         location = require("ts_context_commentstring.utils").get_visual_start_location()
       end
 
       return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = ctx.ctype == require("Comment.utils").ctype.line and "__default" or "__multiline",
+        key = ctx.ctype == require("Comment.utils").ctype.linewise and "__default" or "__multiline",
         location = location,
       }
     end
