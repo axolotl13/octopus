ddb74cc39af4aeae282ffbf0db05b9714dd62844
geovanni <geovani@exinnotech.com>
2023-01-22 16:34:42 -0600

    Se formateo código para gitsigns

--- lua/plugins/_gitsigns.lua
+++ lua/plugins/_gitsigns.lua
@@ -39,7 +39,8 @@ return {
     linehl = false,
     word_diff = false,
     watch_gitdir = {
-      interval = 1000, follow_files = true
+      interval = 1000,
+      follow_files = true
     },
     attach_to_untracked = false,
     current_line_blame = false,
@@ -65,18 +66,39 @@ return {
     }
   },
   keys = {
-    { "<leader>gr", "<cmd>Gitsigns reset_buffer<cr>", desc = "[Gitsigns] Deshacer todos los cambios del archivo actual" },
+    {
+      "<leader>gr",
+      "<cmd>Gitsigns reset_buffer<cr>",
+      desc = "[Gitsigns] Deshacer todos los cambios del archivo actual"
+    },
     { "<leader>gs", "<cmd>Gitsigns stage_buffer<cr>", desc = "[Gitsigns] Poner la línea actual en el área de stage" },
     { "<leader>gu", "<cmd>Gitsigns reset_hunk<cr>", desc = "[Gitsigns] Deshacer cambios en la línea actual" },
     { "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", desc = "[Gitsigns] Previsualizar cambios que se han hecho" },
-    { "<leader>g{", "<cmd>Gitsigns next_hunk<cr>",
-      desc = "[Gitsigns] Moverse hacía el siguiente cambio que se ha realizado" },
-    { "<leader>g}", "<cmd>Gitsigns prev_hunk<cr>",
-      desc = "[Gitsigns] Moverse hacía el anterior cambio que se ha realizado" },
-    { "<leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "[Gitsigns] Ver los cambios posteriores que se habían hecho" },
-    { "<leader>gq", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "[Gitsigns] Salir en el área de stage en la línea actual" },
-    { "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>",
-      desc = "[Gitsigns] Habilitar línea de cambios por usuario" },
+    {
+      "<leader>g{",
+      "<cmd>Gitsigns next_hunk<cr>",
+      desc = "[Gitsigns] Moverse hacía el siguiente cambio que se ha realizado",
+    },
+    {
+      "<leader>g}",
+      "<cmd>Gitsigns prev_hunk<cr>",
+      desc = "[Gitsigns] Moverse hacía el anterior cambio que se ha realizado",
+    },
+    {
+      "<leader>gd",
+      "<cmd>Gitsigns diffthis<cr>",
+      desc = "[Gitsigns] Ver los cambios posteriores que se habían hecho",
+    },
+    {
+      "<leader>gq",
+      "<cmd>Gitsigns undo_stage_hunk<cr>",
+      desc = "[Gitsigns] Salir en el área de stage en la línea actual",
+    },
+    {
+      "<leader>gl",
+      "<cmd>Gitsigns toggle_current_line_blame<cr>",
+      desc = "[Gitsigns] Habilitar línea de cambios por usuario",
+    },
     { "<leader>gh", "<cmd>Gitsigns toggle_deleted<cr>", desc = "[Gitsigns] Ver las líneas borradas" },
     { "<leader>gn", "<cmd>Gitsigns toggle_numhl<cr>", desc = "[Gitsigns] Pintar numeros" },
   }
