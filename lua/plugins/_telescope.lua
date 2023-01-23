5bd9ba0640577739ca70658b125e30159d4fbb6e
geovanni <geovani@exinnotech.com>
2023-01-23 00:01:37 -0600

    Se cambio la forma de configurarión para telescope

--- lua/plugins/_telescope.lua
+++ lua/plugins/_telescope.lua
@@ -1,81 +1,78 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    { "nvim-lua/plenary.nvim" }
+    { "nvim-lua/plenary.nvim" },
   },
   cmd = "Telescope",
-  config = function()
-    local telescope = require("telescope")
-
-    local opts = {
-      defaults = {
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case"
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      },
+      mappings = {
+        i = {
+          ["<esc>"] = require("telescope.actions").close,
+          ["<c-s>"] = require("telescope.actions").select_horizontal,
+          ["<c-v>"] = require("telescope.actions").select_vertical,
+          ["<c-n>"] = require("telescope.actions").cycle_history_next,
+          ["<c-p>"] = require("telescope.actions").cycle_history_prev,
+          ["<c-j>"] = require("telescope.actions").move_selection_next,
+          ["<c-k>"] = require("telescope.actions").move_selection_previous,
+          ["<pageup>"] = require("telescope.actions").results_scrolling_up,
+          ["<pagedown>"] = require("telescope.actions").results_scrolling_down,
+          ["<tab>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_worse,
+          ["<s-tab>"] = require("telescope.actions").toggle_selection
+            + require("telescope.actions").move_selection_better,
         },
-        mappings = {
-          i = {
-            ["<esc>"] = require("telescope.actions").close,
-            ["<c-s>"] = require("telescope.actions").select_horizontal,
-            ["<c-v>"] = require("telescope.actions").select_vertical,
-            ["<c-n>"] = require("telescope.actions").cycle_history_next,
-            ["<c-p>"] = require("telescope.actions").cycle_history_prev,
-            ["<c-j>"] = require("telescope.actions").move_selection_next,
-            ["<c-k>"] = require("telescope.actions").move_selection_previous,
-            ["<pageup>"] = require("telescope.actions").results_scrolling_up,
-            ["<pagedown>"] = require("telescope.actions").results_scrolling_down,
-            ["<tab>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_worse,
-            ["<s-tab>"] = require("telescope.actions").toggle_selection +
-                require("telescope.actions").move_selection_better,
-          }
+      },
+      prompt_prefix = "  ",
+      selection_caret = "  ",
+      entry_prefix = "  ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+          results_width = 0.8,
         },
-        prompt_prefix = "  ",
-        selection_caret = "  ",
-        entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8
-          },
-          vertical = {
-            mirror = false
-          },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120
+        vertical = {
+          mirror = false,
         },
-        file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules" },
-        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-        winblend = 0,
-        border = {},
-        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-        color_devicons = true,
-        use_less = true,
-        path_display = {},
-        set_env = { ["COLORTERM"] = "truecolor" },
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
-      }
-    }
-
-    telescope.setup(opts)
-
-  end,
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+      file_sorter = require("telescope.sorters").get_fuzzy_file,
+      file_ignore_patterns = { "node_modules" },
+      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+      winblend = 0,
+      border = {},
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      color_devicons = true,
+      use_less = true,
+      path_display = {},
+      set_env = { ["COLORTERM"] = "truecolor" },
+      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+    },
+  },
   keys = {
-    { "<leader>sw", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "[Telescope] Buscar palabra dentro del buffer" },
+    {
+      "<leader>sw",
+      "<cmd>Telescope current_buffer_fuzzy_find<cr>",
+      desc = "[Telescope] Buscar palabra dentro del buffer",
+    },
     { "<leader>sx", "<cmd>Telescope command_history<cr>", desc = "[Telescope] Buscar comandos ejecutados" },
     { "<leader>ss", "<cmd>Telescope live_grep<cr>", desc = "[Telescope] Buscar palabra en todas las carpetas" },
     { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "[Telescope] Buscar archivo en todas las carpetas" },
@@ -88,7 +85,10 @@ return {
     { "<leader>sm", "<cmd>Telescope fd cwd=$HOME<cr>", desc = "[Telescope] Buscar en HOME" },
     { "<leader>sn", "<cmd>Telescope notify<cr>", desc = "[Telescope] Buscar últimas notificaciones" },
     { "<leader>sq", "<cmd>Telescope buffers<cr>", desc = "[Telescope] Buscar en buffer actuales" },
-    { "<leader>se", "<cmd>lua require('telescope.builtin').grep_string()<cr>",
-      desc = "[Telescope] Buscar palabra seleccionada" }
-  }
+    {
+      "<leader>se",
+      "<cmd>lua require('telescope.builtin').grep_string()<cr>",
+      desc = "[Telescope] Buscar palabra seleccionada",
+    },
+  },
 }
