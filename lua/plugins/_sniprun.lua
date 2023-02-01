9a1b9250bdcf16582d8e33ed809f724afa93c781
geovanni <geovani@exinnotech.com>
2023-02-01 10:58:02 -0600

    Se corrigio build para sniprun

--- lua/plugins/_sniprun.lua
+++ lua/plugins/_sniprun.lua
@@ -1,6 +1,6 @@
 return {
   "michaelb/sniprun",
-  --[[ build = "bash ./install.sh", ]]
+  build = "bash ./install.sh",
   cmd = "SnipRun",
   opts = {
     interpreter_options = { JS_original = { use_on_filetypes = { "javascriptreact" } } },
