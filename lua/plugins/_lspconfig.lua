6b1385e4ade9b80bdf436b22e661750ea13f9afc
joker <geovannicinto@gmail.com>
2023-03-17 12:52:40 -0600

    Se corrigió lsp para lus_ls

--- lua/plugins/_lspconfig.lua
+++ lua/plugins/_lspconfig.lua
@@ -64,20 +64,22 @@ return {
       pyright = {},
       sqls = {},
       lua_ls = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
           },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
+        }
       },
       tsserver = {},
       yamlls = {},
