3612eee263c7e8d7236a400ad618839ff4949c41
joker <geovannicinto@gmail.com>
2023-04-09 12:59:34 -0600

    Se agrego funcion para nvim-tree

--- lua/octopus/_cmds.lua
+++ lua/octopus/_cmds.lua
@@ -21,3 +21,19 @@ cmd("BufEnter", { pattern = "*", command = "set fo-=c fo-=r fo-=o" })
 
 -- Deshabilitar los números de línea en la terminal
 cmd("TermOpen", { group = init_group, command = "setlocal nonumber" })
+
+-- Auto cerrar NvimTree
+-- cmd("BufEnter", {
+--   group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+--   pattern = "NvimTree_*",
+--   callback = function()
+--     local layout = vim.api.nvim_call_function("winlayout", {})
+--     if
+--       layout[1] == "leaf"
+--       and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+--       and layout[3] == nil
+--     then
+--       vim.api.nvim_command [[confirm quit]]
+--     end
+--   end,
+-- })
