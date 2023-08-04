eab44810b8cd66ae87641f5f4493772002b8e1a9
joker <geovannicinto@gmail.com>
2023-08-04 13:03:11 -0600

    Se código innecesario

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
-if vim.fn.has("nvim-0.8") ~= 1 then
-  vim.notify("Please upgrade your Neovim base installation", vim.log.levels.WARN)
-  vim.wait(5000, function()
-    return false
-  end)
-  vim.cmd "cquit"
-end
-
-require("octopus")
+require "octopus"
