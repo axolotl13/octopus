0eaefde792186465a916395429ca0cda75d3d90f
joker <geovannicinto@gmail.com>
2023-09-07 12:41:08 -0600

    Se elimino varias opciones que ya estaban por default

--- lua/plugins/_autopairs.lua
+++ lua/plugins/_autopairs.lua
@@ -1,32 +1,16 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  config = function()
-    local pairs = require("nvim-autopairs")
-    local opts = {
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-        java = false
-      },
-      disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
-      fast_wrap = {
-        map = "<M-e>", -- M = Alt
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0,
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-      },
-    }
-    pairs.setup(opts)
-    require("cmp").event:on(
-      "confirm_done",
-      require("nvim-autopairs.completion.cmp").on_confirm_done({ tex = "" })
-    )
+  opts = {
+    check_ts = true,
+    disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
+    fast_wrap = {
+      map = "<M-e>", -- M = Alt
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    },
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done { tex = "" })
   end,
 }
