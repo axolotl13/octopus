b93337c6f588087d23a9d6a27c6418a68caf6bb5
joker <geovannicinto@gmail.com>
2023-09-09 00:33:33 -0600

    Se limpio código

--- lua/plugins/_diffview.lua
+++ lua/plugins/_diffview.lua
@@ -1,6 +1,6 @@
 return {
   "sindrets/diffview.nvim",
-  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
   dependencies = {
     { "nvim-lua/plenary.nvim" },
   },
@@ -9,19 +9,9 @@ return {
       folder_closed = require("ui.icons").explorer.default,
       folder_open = require("ui.icons").explorer.open,
     },
-    signs = {
-      fold_closed = require("ui.icons").explorer.arrow_closed,
-      fold_open = require("ui.icons").explorer.arrow_open,
-    },
     file_panel = {
-      listing_style = "tree",
       tree_options = {
-        flatten_dirs = true,
         folder_statuses = "always",
-      },
-      win_config = {
-        position = "left",
-        width = 35,
       },
     },
     file_history_panel = {
@@ -30,29 +20,13 @@ return {
           single_file = {
             diff_merges = "combined",
           },
-          multi_file = {
-            diff_merges = "first-parent",
-          },
         },
       },
-      win_config = {
-        position = "bottom",
-        height = 16,
-      },
-    },
-    commit_log_panel = {
-      win_config = {},
-    },
-    default_args = {
-      DiffviewOpen = {},
-      DiffviewFileHistory = {},
-    },
-    keymaps = {
-      disable_defaults = false,
     },
   },
   keys = {
     { "<leader>dd", "<cmd>DiffviewOpen<cr>", desc = "[Diffview] Abre Diffview" },
+    { "<leader>db", "<cmd>DiffviewOpen %<cr>", desc = "[Diffview] Abre Diffview en el buffer actual" },
     { "<leader>dq", "<cmd>DiffviewClose<cr>", desc = "[Diffview] Cierra Diffview" },
     { "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "[Diffview] Abre DiffviewFileHistory" },
     { "<leader>dt", "<cmd>DiffviewToggleFiles<cr>", desc = "[Diffview] DiffviewToggleFiles" },
