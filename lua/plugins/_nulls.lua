c046257242d9754342505e8a70a7606aa7c373d8
joker <geovannicinto@gmail.com>
2023-09-09 21:38:28 -0600

    Se cambio sqlfluff por sql_formatter

--- lua/plugins/_nulls.lua
+++ lua/plugins/_nulls.lua
@@ -1,8 +1,8 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  event = "BufReadPre",
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local nls = require("null-ls")
+    local nls = require "null-ls"
     local opts = {
       debounce = 150,
       save_after_format = false,
@@ -11,28 +11,34 @@ return {
         nls.builtins.formatting.black,
         nls.builtins.formatting.isort,
         nls.builtins.formatting.stylua,
-        nls.builtins.formatting.shfmt.with({ extra_args = { "-i", "4" } }),
+        nls.builtins.formatting.shfmt.with { extra_args = { "-i", "4" } },
         nls.builtins.formatting.xmllint,
-        nls.builtins.formatting.sqlfluff.with({ extra_args = { "--dialect", "mysql" } }),
-        nls.builtins.formatting.codespell.with({ filetypes = { "markdown" } }),
-        nls.builtins.formatting.clang_format.with({
-          filetypes = { "c", "cpp", "cs", "java" }, extra_args = { "-style", "{IndentWidth: 4}" } } ),
+        nls.builtins.formatting.sql_formatter.with {
+          extra_args = { "--config", "/home/joker/.sql_formatter.json" },
+        },
+        nls.builtins.formatting.codespell.with { filetypes = { "markdown" } },
+        nls.builtins.formatting.clang_format.with {
+          filetypes = { "c", "cpp", "cs", "java" },
+          extra_args = { "-style", "{IndentWidth: 4}" },
+        },
         nls.builtins.diagnostics.tidy,
-        nls.builtins.diagnostics.checkstyle.with({ extra_args = { "-c", "/google_checks.xml" }}),
+        nls.builtins.diagnostics.checkstyle.with { extra_args = { "-c", "/google_checks.xml" } },
         nls.builtins.diagnostics.markdownlint,
         nls.builtins.diagnostics.hadolint,
         nls.builtins.diagnostics.eslint_d,
         nls.builtins.diagnostics.jsonlint,
         nls.builtins.diagnostics.luacheck,
-        nls.builtins.diagnostics.sqlfluff.with({ extra_args = { "--dialect", "mysql" } }),
+        nls.builtins.diagnostics.sqlfluff.with {
+          extra_args = { "--config", "/home/joker/.sqlfluff" },
+        },
         nls.builtins.diagnostics.shellcheck,
         nls.builtins.diagnostics.yamllint,
         nls.builtins.diagnostics.stylelint,
-        nls.builtins.diagnostics.pylint.with({
+        nls.builtins.diagnostics.pylint.with {
           diagnostics_postprocess = function(diagnostic)
             diagnostic.code = diagnostic.message_id
           end,
-        }),
+        },
         nls.builtins.code_actions.gitsigns,
       },
     }
