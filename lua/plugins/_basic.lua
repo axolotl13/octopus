cb09e318c1ca8ddf730d92cdac4666d6eea81a9b
joker <geovannicinto@gmail.com>
2023-09-10 23:24:52 -0600

    Se añadio nuevo plugin statuscol

--- lua/plugins/_basic.lua
+++ lua/plugins/_basic.lua
@@ -66,7 +66,29 @@ return {
       hlgroup = "MiniCursorword",
     },
   },
-
+  {
+    "luukvbaal/statuscol.nvim",
+    event = "VeryLazy",
+    opts = function()
+      local builtin = require "statuscol.builtin"
+      return {
+        relculright = true,
+        segments = {
+          { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          {
+            sign = {
+              name = { "GitSigns", "Diagnostic" },
+              maxwidth = 1,
+              colwidth = 1,
+              auto = false,
+            },
+            click = "v.lua.ScSa",
+          },
+        },
+      }
+    end,
+  },
   -- Vim plugins
   {
     "tpope/vim-fugitive",
