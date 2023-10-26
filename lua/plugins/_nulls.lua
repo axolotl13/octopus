0567cd65230575af69623a27089e9a0962f52db6
joker <geovannicinto@gmail.com>
2023-10-26 15:55:31 -0600

    Se cambio null-ls a none-ls

--- lua/plugins/_nulls.lua
+++ lua/plugins/_nulls.lua
@@ -1,6 +1,6 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  "nvimtools/none-ls.nvim",
+  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   config = function()
     local nls = require "null-ls"
     local opts = {
@@ -16,7 +16,10 @@ return {
         nls.builtins.formatting.sql_formatter.with {
           extra_args = { "--config", "/home/joker/.sql_formatter.json" },
         },
-        nls.builtins.formatting.codespell.with { filetypes = { "markdown" } },
+        nls.builtins.formatting.codespell.with {
+          extra_args = { "-L", "hace,utiliza,posible,caracteres,conector" },
+          filetypes = { "markdown" },
+        },
         nls.builtins.formatting.clang_format.with {
           filetypes = { "c", "cpp", "cs", "java" },
           extra_args = { "-style", "{IndentWidth: 4}" },
@@ -31,7 +34,7 @@ return {
         nls.builtins.diagnostics.sqlfluff.with {
           extra_args = { "--config", "/home/joker/.sqlfluff" },
         },
-        nls.builtins.diagnostics.shellcheck,
+        -- nls.builtins.diagnostics.shellcheck,
         nls.builtins.diagnostics.yamllint,
         nls.builtins.diagnostics.stylelint,
         nls.builtins.diagnostics.pylint.with {
