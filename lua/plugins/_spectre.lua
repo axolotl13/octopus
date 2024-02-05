345032d708af32cb6269c16a7b84c92bc72f45f9
joker <geovannicinto@gmail.com>
2024-02-04 19:34:02 -0600

    feat(spectre): Se elimino keybinds innecesarios

--- lua/plugins/_spectre.lua
+++ lua/plugins/_spectre.lua
@@ -10,25 +10,10 @@ return {
       ["enter_file"] = {
         map = "<cr>",
         cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
-        "Ir al archivo actual",
-      },
-      ["send_to_qf"] = {
-        map = "<leader>q",
-        cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
-        "Enviar todas las líneas a quickfix",
-      },
-      ["replace_cmd"] = {
-        map = "<leader>c",
-        cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
-        "Reemplazar con comando Vim",
-      },
-      ["show_option_menu"] = {
-        map = "<leader>o",
-        cmd = "<cmd>lua require('spectre').show_options()<CR>",
-        "Mostrar opciones",
+        "Abrir archivo",
       },
       ["run_current_replace"] = {
-        map = "<leader>r",
+        map = "<leader>rc",
         cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
         "Reemplazar línea actual",
       },
@@ -36,26 +21,6 @@ return {
         map = "<leader>ra",
         cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
         "Reemplazar todas las líneas",
-      },
-      ["change_view_mode"] = {
-        map = "<leader>v",
-        cmd = "<cmd>lua require('spectre').change_view()<CR>",
-        "Cambia el modo de visualización de los resultados",
-      },
-      ["change_replace_sed"] = {
-        map = "ts",
-        cmd = "<cmd>lua require('spectre').change_engine_replace('sed')<CR>",
-        "Usar sed para reemplazar",
-      },
-      ["toggle_live_update"] = {
-        map = "tu",
-        cmd = "<cmd>lua require('spectre').toggle_live_update()<CR>",
-        "update change when vim write file.",
-      },
-      ["toggle_ignore_case"] = {
-        map = "ti",
-        cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
-        "Alternar ignore case",
       },
       ["toggle_ignore_hidden"] = {
         map = "th",
