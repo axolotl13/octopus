47e69198c958e7adfd88cf253b908e4d1dcd16fb
joker <geovannicinto@gmail.com>
2024-02-07 02:04:27 -0600

    refactor(windows): Se cambiaron keybinds

--- lua/plugins/_windows.lua
+++ lua/plugins/_windows.lua
@@ -11,17 +11,17 @@ return {
       winwidth = 30,
     },
     ignore = {
-      filetype = { "NvimTree", "spectre_panel", "DiffviewFiles" },
+      filetype = { "NvimTree", "spectre_panel", "DiffviewFiles", "gitcommit" },
     },
     animation = {
       enable = true,
-      duration = 220,
+      duration = 150,
       fps = 60,
     },
   },
   keys = {
+    { "<leader>we", "<cmd>WindowsEqualize<cr>", desc = "[Windows] Igualar ventanas" },
+    { "<leader>wm", "<cmd>WindowsMaximize<cr>", desc = "[Windows] Maximizar ventana actual" },
     { "<leader>wq", "<cmd>WindowsToggleAutowidth<cr>", desc = "[Windows] Des/habilitar windows" },
-    { "<leader>we", "<cmd>WindowsMaximize<cr>", desc = "[Windows] Maximizar ventana actual" },
-    { "<leader>ww", "<cmd>WindowsEqualize<cr>", desc = "[Windows] Igualar ventanas" },
   },
 }
