6a8dfcbea7a3200ea79760d628d2c3d6e4b50476
joker <geovannicinto@gmail.com>
2024-02-07 01:42:29 -0600

    refactor(sniprun): Se formateo el codígo

--- lua/plugins/_sniprun.lua
+++ lua/plugins/_sniprun.lua
@@ -1,16 +1,13 @@
 return {
   "michaelb/sniprun",
-  build = "bash ./install.sh",
-  cmd = "SnipRun",
+  build = "sh install.sh",
   opts = {
-    interpreter_options = { JS_original = { use_on_filetypes = { "javascriptreact" } } },
     display = { "VirtualTextOK", "NvimNotify" },
     display_options = { notification_timeout = 5 },
   },
   keys = {
-    { "<leader>re", "<cmd>SnipRun<cr>", desc = "[SnipRun] Ejecutar línea de código" },
+    { "<leader>re", mode = { "n", "x" }, ":SnipRun<cr>", desc = "[SnipRun] Ejecutar código" },
     { "<leader>rr", "<cmd>SnipReset<cr>", desc = "[SnipRun] Reiniciar SnipRun" },
     { "<leader>rq", "<cmd>SnipClose<cr>", desc = "[SnipRun] Cerrar SnipRun" },
-    { "<leader>re", mode = { "x" }, ":SnipRun<cr>", desc = "[SnipRun] Ejecutar bloque de código" },
   },
 }
