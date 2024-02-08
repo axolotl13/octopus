356a126c48cc0a03bbfee2e4953593ef20b2cc09
joker <geovannicinto@gmail.com>
2024-02-07 23:23:15 -0600

    refactor(session): Se agrego init

--- lua/plugins/_session.lua
+++ lua/plugins/_session.lua
@@ -1,6 +1,30 @@
 return {
   "stevearc/resession.nvim",
   lazy = false,
+  init = function()
+    local resession = require "resession"
+    local function get_session_name()
+      local name = vim.fn.getcwd()
+      local branch = vim.trim(vim.fn.system "git branch --show-current")
+      if vim.v.shell_error == 0 then
+        return name .. branch
+      else
+        return name
+      end
+    end
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        if vim.fn.argc(-1) == 0 then
+          resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      callback = function()
+        resession.save(get_session_name(), { dir = "dirsession", notify = false })
+      end,
+    })
+  end,
   opts = {
     dir = "resession",
     autosave = {
@@ -27,31 +51,6 @@ return {
     },
     extensions = {},
   },
-  config = function(_, opts)
-    local resession = require "resession"
-    resession.setup(opts)
-    local function get_session_name()
-      local name = vim.fn.getcwd()
-      local branch = vim.trim(vim.fn.system "git branch --show-current")
-      if vim.v.shell_error == 0 then
-        return name .. branch
-      else
-        return name
-      end
-    end
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        if vim.fn.argc(-1) == 0 then
-          resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
-        end
-      end,
-    })
-    vim.api.nvim_create_autocmd("VimLeavePre", {
-      callback = function()
-        resession.save(get_session_name(), { dir = "dirsession", notify = false })
-      end,
-    })
-  end,
   keys = {
     { "<leader>xs", "<cmd>lua require('resession').save()<cr>", desc = "[resession] Guardar Sesión" },
     { "<leader>xl", "<cmd>lua require('resession').load()<cr>", desc = "[resession] Cargar Sesión" },
