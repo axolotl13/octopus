de8f2ee0fe4a858f6f20f3f2b0c9ef89df3ec8af
joker <geovannicinto@gmail.com>
2024-02-08 18:23:53 -0600

    feat(venv-selector): Se inicia automaticamente cuando hay un archivo venv

--- lua/plugins/_basic.lua
+++ lua/plugins/_basic.lua
@@ -39,11 +39,21 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
-    ft = { "python" },
-    opts = {
-      name = { "venv", ".venv" },
-    },
-    keys = { { "<leader>vv", "<cmd>VenvSelect<cr>", desc = "Selecciona el entorno virtual de Python" } },
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        desc = "Auto select virtualenv",
+        pattern = "*",
+        callback = function()
+          local venv = vim.fn.finddir("venv", vim.fn.getcwd() .. ";")
+          if venv ~= "" then
+            require("venv-selector").retrieve_from_cache()
+          end
+        end,
+        once = true,
+      })
+    end,
+    keys = { { "<leader>vv", "<cmd>VenvSelect<cr>", desc = "[Venv] Selecciona el entorno virtual de Python" } },
+    opts = { name = { "venv", ".venv" } },
   },
   {
     "CRAG666/code_runner.nvim",
