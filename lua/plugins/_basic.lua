1578cfaf642a18a71f830418659537d0ed1026f7
joker <geovannicinto@gmail.com>
2024-02-14 23:50:45 -0600

    refactor(venv): Se añadio archivo independiente para este plugin

--- lua/plugins/_basic.lua
+++ lua/plugins/_basic.lua
@@ -37,25 +37,6 @@ return {
     },
   },
   {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
-    init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
-        desc = "Auto select virtualenv",
-        pattern = "*",
-        callback = function()
-          local venv = vim.fn.finddir("venv", vim.fn.getcwd() .. ";")
-          if venv ~= "" then
-            require("venv-selector").retrieve_from_cache()
-          end
-        end,
-        once = true,
-      })
-    end,
-    keys = { { "<leader>vv", "<cmd>VenvSelect<cr>", desc = "[Venv] Selecciona el entorno virtual de Python" } },
-    opts = { name = { "venv", ".venv" } },
-  },
-  {
     "CRAG666/code_runner.nvim",
     config = true,
     cmd = { "RunCode", "RunFile" },
