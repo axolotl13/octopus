9ae70eff480c4e44c8ab4793d3943f081139d1b2
joker <geovannicinto@gmail.com>
2024-03-16 23:33:50 -0600

    refactor(cmds): Se cambio autocmd

--- lua/octopus/_cmds.lua
+++ lua/octopus/_cmds.lua
@@ -1,13 +1,13 @@
-local cmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Especificar sangría para ciertos tipos de archivo
-cmd("FileType", { pattern = "cpp", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "python", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "java", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "sql", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "xml", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "sh", command = "setlocal shiftwidth=4 tabstop=4" })
-cmd("FileType", { pattern = "markdown", command = "setlocal spell colorcolumn=80" })
+autocmd("FileType", { pattern = "cpp", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "python", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "java", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "sql", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "xml", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "sh", command = "setlocal shiftwidth=4 tabstop=4" })
+autocmd("FileType", { pattern = "markdown", command = "setlocal spell colorcolumn=80" })
 
 -- Guardado automático
 -- cmd({"InsertLeave", "TextChanged"}, {
@@ -16,23 +16,8 @@ cmd("FileType", { pattern = "markdown", command = "setlocal spell colorcolumn=80
 -- })
 
 -- Deshabilitar auto comentario
-cmd("BufEnter", { pattern = "*", command = "set fo-=c fo-=r fo-=o" })
+autocmd("BufEnter", { pattern = "*", command = "setlocal fo-=c fo-=r fo-=o" })
 
 -- Deshabilitar los números de línea en la terminal
-cmd("TermOpen", { group = init_group, command = "setlocal nonumber" })
-
--- Auto cerrar NvimTree
--- cmd("BufEnter", {
---   group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
---   pattern = "NvimTree_*",
---   callback = function()
---     local layout = vim.api.nvim_call_function("winlayout", {})
---     if
---       layout[1] == "leaf"
---       and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
---       and layout[3] == nil
---     then
---       vim.api.nvim_command [[confirm quit]]
---     end
---   end,
--- })
+autocmd("TermOpen", { pattern = "*", command = "setlocal nonumber norelativenumber" })
+autocmd("TermOpen", { pattern = "*", command = "startinsert" })
