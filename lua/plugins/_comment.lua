d79ab09b0c96f94c4d6f5243b429c6e964b48cc7
joker <geovannicinto@gmail.com>
2024-03-17 13:25:04 -0600

    refactor(comment): Minimo cambio

--- lua/plugins/_comment.lua
+++ lua/plugins/_comment.lua
@@ -4,9 +4,7 @@ return {
   opts = function()
     return {
       ignore = "^$",
-      mappings = {
-        extra = false,
-      },
+      mappings = { extra = false },
       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     }
   end,
