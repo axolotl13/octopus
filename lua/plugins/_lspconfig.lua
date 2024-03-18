a4c6046f35476b05dc010a0ecbcd8befac74fb34
joker <geovannicinto@gmail.com>
2024-03-18 00:02:27 -0600

    refactor(lspconfig): Se separo plugins

--- lua/plugins/_lspconfig.lua
+++ lua/plugins/_lspconfig.lua
@@ -1,182 +1,199 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
+  {
     "dnlhc/glance.nvim",
-    {
-      "williamboman/mason.nvim",
-      dependencies = { "williamboman/mason-lspconfig.nvim" },
-      opts = {
-        ui = {
-          icons = { package_installed = "", package_pending = "", package_uninstalled = "ﮊ" },
-          keymaps = { uninstall_package = "d" },
-        },
-      },
-      keys = { { "<leader>mm", "<cmd>Mason<cr>", desc = "[Mason] Abrir Mason" } },
+    keys = {
+      { "gd", "<cmd>Glance definitions<cr>" },
+      { "gi", "<cmd>Glance implementations<cr>" },
+      { "gt", "<cmd>Glance type_definitions<cr>" },
+      { "gr", "<cmd>Glance references<cr>" },
     },
   },
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  opts = {
-    diagnostics = {
-      underline = true,
-      update_in_insert = false,
-      virtual_text = {
-        prefix = "",
-      },
-      severity_sort = true,
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
     },
-    signs = {
-      Error = require("ui.icons").dg.bugf,
-      Warn = require("ui.icons").dg.bugf,
-      Hint = require("ui.icons").dg.hint,
-      Info = require("ui.icons").dg.bugf,
-    },
-    servers = {
-      bashls = {},
-      clangd = {},
-      cssls = {},
-      docker_compose_language_service = {},
-      dockerls = {},
-      ember = {
-        filetypes = {
-          "handlebars",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    opts = {
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          prefix = "",
         },
+        severity_sort = true,
       },
-      emmet_language_server = {},
-      -- eslint = {},
-      -- gopls = {},
-      html = {},
-      intelephense = {},
-      jdtls = {},
-      jsonls = {},
-      lemminx = {},
-      lua_ls = {
-        settings = {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-              enable = false,
+      signs = {
+        Error = require("ui.icons").dg.bugf,
+        Warn = require("ui.icons").dg.bugf,
+        Hint = require("ui.icons").dg.hint,
+        Info = require("ui.icons").dg.bugf,
+      },
+      servers = {
+        bashls = {},
+        clangd = {},
+        cssls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        ember = {
+          filetypes = {
+            "handlebars",
+          },
+        },
+        emmet_language_server = {},
+        -- eslint = {},
+        -- gopls = {},
+        html = {},
+        intelephense = {},
+        jdtls = {},
+        jsonls = {},
+        lemminx = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = {
+                enable = false,
+              },
             },
           },
         },
+        marksman = {},
+        nginx_language_server = {},
+        pyright = {},
+        solargraph = {},
+        rust_analyzer = {},
+        sqls = {},
+        texlab = {},
+        tsserver = {},
+        yamlls = {},
       },
-      marksman = {},
-      nginx_language_server = {},
-      pyright = {},
-      solargraph = {},
-      rust_analyzer = {},
-      sqls = {},
-      texlab = {},
-      tsserver = {},
-      yamlls = {},
     },
-  },
-  config = function(_, opts)
-    local on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
+    config = function(_, opts)
+      local on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
 
-      if client.server_capabilities.documentSymbolProvider then
-        require("nvim-navic").attach(client, bufnr)
+        if client.server_capabilities.documentSymbolProvider then
+          require("nvim-navic").attach(client, bufnr)
+        end
+
+        local keymap = vim.keymap.set
+        local bufn = { noremap = true, silent = true, buffer = bufnr }
+
+        -- keymap("n", "gd", vim.lsp.buf.definition, bufn)
+        -- keymap("n", "gi", vim.lsp.buf.declaration, bufn)
+        keymap("n", "gD", vim.lsp.buf.declaration, bufn)
+        keymap("n", "K", vim.lsp.buf.hover, bufn)
+        -- keymap("n", "gt", vim.lsp.buf.type_definition, bufn)
+        keymap("n", "gn", vim.lsp.buf.rename, bufn)
+        keymap("n", "gp", vim.lsp.buf.code_action, bufn)
+        -- keymap("n", "gr", vim.lsp.buf.references, bufn)
+        keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufn)
+        keymap("n", "<leader>wd", vim.lsp.buf.remove_workspace_folder, bufn)
+        keymap("n", "<leader>wl", function()
+          vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders()), vim.log.levels.INFO)
+        end, bufn)
+        keymap("n", "<leader>ff", function()
+          vim.lsp.buf.format { async = true }
+        end, bufn)
+        --[[ keymap("x", "<leader>ff", vim.lsp.buf.range_formatting, bufn) ]]
+        keymap("n", "gs", vim.lsp.buf.signature_help, bufn)
       end
 
-      local keymap = vim.keymap.set
-      local bufn = { noremap = true, silent = true, buffer = bufnr }
+      -- diagnostics
+      for name, icon in pairs(opts.signs) do
+        local hl = "DiagnosticSign" .. name
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
 
-      keymap("n", "gd", "<cmd>Glance definitions<cr>", bufn)
-      keymap("n", "gi", "<cmd>Glance implementations<cr>", bufn)
-      keymap("n", "gD", vim.lsp.buf.declaration, bufn)
-      keymap("n", "K", vim.lsp.buf.hover, bufn)
-      keymap("n", "gt", "<cmd>Glance type_definitions<cr>", bufn)
-      keymap("n", "gn", vim.lsp.buf.rename, bufn)
-      keymap("n", "gp", vim.lsp.buf.code_action, bufn)
-      keymap("n", "gr", "<cmd>Glance references<cr>", bufn)
-      keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufn)
-      keymap("n", "<leader>wd", vim.lsp.buf.remove_workspace_folder, bufn)
-      keymap("n", "<leader>wl", function()
-        vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders()), vim.log.levels.INFO)
-      end, bufn)
-      keymap("n", "<leader>ff", function()
-        vim.lsp.buf.format { async = true }
-      end, bufn)
-      --[[ keymap("x", "<leader>ff", vim.lsp.buf.range_formatting, bufn) ]]
-      keymap("n", "gs", vim.lsp.buf.signature_help, bufn)
-    end
+      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
-    -- diagnostics
-    for name, icon in pairs(opts.signs) do
-      local hl = "DiagnosticSign" .. name
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+      -- servers
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities = vim.lsp.protocol.make_client_capabilities()
+      -- capabilities.offsetEncoding = {"utf-16"} Solución para utf en C++
+      capabilities.textDocument.completion.completionItem = {
+        commitCharactersSupport = true,
+        deprecatedSupport = true,
+        preselectSupport = true,
+        snippetSupport = true,
+      }
 
-    vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+      local ensure_installed = {}
+      local servers = opts.servers
 
-    -- servers
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    capabilities = vim.lsp.protocol.make_client_capabilities()
-    -- capabilities.offsetEncoding = {"utf-16"} Solución para utf en C++
-    capabilities.textDocument.completion.completionItem = {
-      commitCharactersSupport = true,
-      deprecatedSupport = true,
-      preselectSupport = true,
-      snippetSupport = true,
-    }
+      local options = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
 
-    local ensure_installed = {}
-    local servers = opts.servers
+      for server, server_opts in pairs(servers) do
+        server_opts = vim.tbl_deep_extend("force", {}, options, server_opts or {})
+        require("lspconfig")[server].setup(server_opts)
+        ensure_installed[#ensure_installed + 1] = server
+      end
 
-    local options = {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-
-    for server, server_opts in pairs(servers) do
-      server_opts = vim.tbl_deep_extend("force", {}, options, server_opts or {})
-      require("lspconfig")[server].setup(server_opts)
-      ensure_installed[#ensure_installed + 1] = server
-    end
-
-    -- Mason
-    local mlsp = require "mason-lspconfig"
-    mlsp.setup { ensure_installed = ensure_installed, automatic_installation = true }
-  end,
-  keys = {
-    {
-      "gf",
-      function()
-        vim.diagnostic.open_float()
-      end,
-      desc = "[Diagnostics] Diagnostico flotante",
+      -- Mason
+      local mason_lsp = require "mason-lspconfig"
+      mason_lsp.setup {
+        ensure_installed = ensure_installed,
+        automatic_installation = true,
+      }
+    end,
+    keys = {
+      {
+        "gf",
+        function()
+          vim.diagnostic.open_float()
+        end,
+        desc = "[Diagnostics] Diagnostico flotante",
+      },
+      {
+        "g{",
+        function()
+          vim.diagnostic.goto_prev()
+        end,
+        desc = "[Diagnostics] Ir al anterior diagnostico",
+      },
+      {
+        "g}",
+        function()
+          vim.diagnostic.goto_next()
+        end,
+        desc = "[Diagnostics] Ir al siguiente diagnostico",
+      },
+      {
+        "gk",
+        function()
+          vim.diagnostic.setloclist()
+        end,
+        desc = "[Diagnostics] Mostrar mensajes de diagnosticos en quickfix",
+      },
     },
-    {
-      "g{",
-      function()
-        vim.diagnostic.goto_prev()
-      end,
-      desc = "[Diagnostics] Ir al anterior diagnostico",
+  },
+  {
+    "williamboman/mason.nvim",
+    dependencies = { "williamboman/mason-lspconfig.nvim", lazy = true },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "",
+          package_pending = "",
+          package_uninstalled = "",
+        },
+        keymaps = { uninstall_package = "d" },
+      },
     },
-    {
-      "g}",
-      function()
-        vim.diagnostic.goto_next()
-      end,
-      desc = "[Diagnostics] Ir al siguiente diagnostico",
-    },
-    {
-      "gk",
-      function()
-        vim.diagnostic.setloclist()
-      end,
-      desc = "[Diagnostics] Mostrar mensajes de diagnosticos en quickfix",
-    },
+    keys = { { "<leader>ma", "<cmd>Mason<cr>", desc = "[Mason] Abrir Mason" } },
   },
 }
