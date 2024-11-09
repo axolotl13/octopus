94e3ca3688e3b1d12f70bd0f7cb2c7dd70686861
joker <geovannicinto@gmail.com>
2024-11-09 17:51:48 -0600

    feat(treesitter): show code context

--- lua/plugins/_treesitter.lua
+++ lua/plugins/_treesitter.lua
@@ -1,5 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = { max_lines = 5 }
+    }
+  },
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
   opts = {
