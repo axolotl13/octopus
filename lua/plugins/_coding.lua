b45842c0575e97d1c3756d68f01c6720408c53a4
joker <geovannicinto@gmail.com>
2024-11-08 22:56:42 -0600

    feat(nvim-cmp): add snippet engine luasnip

--- lua/plugins/_coding.lua
+++ lua/plugins/_coding.lua
@@ -1,8 +1,25 @@
 return {
 	{
+		"L3MON4D3/LuaSnip",
+		lazy = true,
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		build = "make install_jsregexp",
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+			region_check_events = "CursorMoved",
+		},
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
+			{ "saadparwaiz1/cmp_luasnip", lazy = true },
 			{ "hrsh7th/cmp-buffer", lazy = true },
 			{ "hrsh7th/cmp-path", lazy = true },
 			{ "hrsh7th/cmp-nvim-lsp", lazy = true },
@@ -11,11 +28,17 @@ return {
 			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			local icons = require("octopus._icons").vs
 
 			local opts = {
 				completion = {
 					completeopt = "menu,menuone,noinsert",
+				},
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
 				},
 				formatting = {
 					format = function(_, item)
@@ -27,6 +50,7 @@ return {
 				},
 				sources = {
 					{ name = "nvim_lsp", priority = 1000 },
+					{ name = "luasnip", priority = 750 },
 					{ name = "buffer", priority = 500, group_index = 2 },
 					{ name = "path", priority = 250 },
 				},
@@ -55,6 +79,8 @@ return {
 					["<tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
@@ -62,6 +88,8 @@ return {
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
 						else
 							fallback()
 						end
