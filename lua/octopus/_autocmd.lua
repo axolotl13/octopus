3ed76fda9f0fafb3ca1a8152085f77b27ef40897
joker <geovannicinto@gmail.com>
2025-03-08 00:28:53 -0600

    feat: save and restore view for files
    This commit introduces automatic saving and restoring of the view
    (cursor position, folds, etc.) for files when they are closed and
    reopened.
    
     The following changes were made:
     - Removed the old `BufReadPost` autocmd.
     - Added `BufWinLeave`, `BufWritePost`, and `WinLeave` autocmds to save
    the view using `mkview`.
     - Added `BufWinEnter` autocmd to load the view using `loadview`.
     - Views are only saved/loaded for real files (i.e., those with a
    filetype and not a special buftype).
     - Git commits, git rebases, SVGs, and HG commits are ignored.

--- lua/octopus/_autocmd.lua
+++ lua/octopus/_autocmd.lua
@@ -43,17 +43,25 @@ autocmd("BufEnter", {
   end,
 })
 
-autocmd("BufReadPost", {
-  desc = "Jump to the last place you’ve visited in a file before exiting",
-  callback = function()
-    local ignore_ft = { "gitcommit", "toggleterm", "lazy" }
-    local ft = vim.bo.filetype
-    if not vim.tbl_contains(ignore_ft, ft) then
-      local mark = vim.api.nvim_buf_get_mark(0, '"')
-      local lcount = vim.api.nvim_buf_line_count(0)
-      if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        vim.cmd.normal { args = { "zz" }, bang = true }
+autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
+  desc = "Save view with mkview for real files",
+  callback = function(args)
+    if vim.b[args.buf].view_activated then
+      vim.cmd.mkview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+
+autocmd("BufWinEnter", {
+  desc = "Try to load file view if available and enable view saving for real files",
+  callback = function(args)
+    if not vim.b[args.buf].view_activated then
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+      local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+      local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit" }
+      if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+        vim.b[args.buf].view_activated = true
+        vim.cmd.loadview { mods = { emsg_silent = true } }
       end
     end
   end,
