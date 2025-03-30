95dc6202db4b5b46263d3cdd9c7da4b87f6b1ea6
joker <geovannicinto@gmail.com>
2025-03-30 13:08:00 -0600

    feat(editor): configure flash.nvim and snacks.nvim
    This commit configures flash.nvim and snacks.nvim by:
    - Adding "snacks_notif" and "snacks_notif_history" to the disabled plugins list.
    - Adding configuration for snacks.nvim with custom keymaps and actions.
    - Modifying the flash.nvim keymaps.
    - Adding flash toggle command in command mode.

--- lua/plugins/_editor.lua
+++ lua/plugins/_editor.lua
@@ -104,6 +104,8 @@ return {
           "codecompanion",
           "grug-far",
           "notify",
+          "snacks_notif",
+          "snacks_notif_history",
           "terminal",
           "fugitive",
           "flash_prompt",
@@ -118,43 +120,6 @@ return {
         },
       },
     },
-    specs = {
-      {
-        "folke/snacks.nvim",
-        opts = {
-          picker = {
-            win = {
-              input = {
-                keys = {
-                  ["<a-s>"] = { "flash", mode = { "n", "i" } },
-                  ["s"] = { "flash" },
-                },
-              },
-            },
-            actions = {
-              flash = function(picker)
-                require("flash").jump {
-                  pattern = "^",
-                  label = { after = { 0, 0 } },
-                  search = {
-                    mode = "search",
-                    exclude = {
-                      function(win)
-                        return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
-                      end,
-                    },
-                  },
-                  action = function(match)
-                    local idx = picker.list:row2idx(match.pos[1])
-                    picker.list:_move(idx, true, true)
-                  end,
-                }
-              end,
-            },
-          },
-        },
-      },
-    },
     keys = {
       {
         "s",
@@ -165,12 +130,12 @@ return {
         desc = "Flash",
       },
       {
-        "S",
-        mode = { "n", "o", "x" },
+        "<c-s>",
+        mode = { "c" },
         function()
-          require("flash").treesitter()
+          require("flash").toggle()
         end,
-        desc = "Flash treesitter",
+        desc = "Toggle Flash Search",
       },
     },
   },
@@ -417,6 +382,41 @@ return {
           Snacks.zen()
         end,
         desc = "Toggle Zen Mode",
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<a-s>"] = { "flash", mode = { "n", "i" } },
+              ["s"] = { "flash" },
+            },
+          },
+        },
+        actions = {
+          flash = function(picker)
+            require("flash").jump {
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                  end,
+                },
+              },
+              action = function(match)
+                local idx = picker.list:row2idx(match.pos[1])
+                picker.list:_move(idx, true, true)
+              end,
+            }
+          end,
+        },
       },
     },
   },
