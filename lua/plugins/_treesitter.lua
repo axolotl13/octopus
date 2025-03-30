a33a5d24d94dd2e9063ddaa241da2d3405e2f55d
joker <geovannicinto@gmail.com>
2025-03-30 12:04:06 -0600

    feat(treesitter): enable multiwindow support for context provider

--- lua/plugins/_treesitter.lua
+++ lua/plugins/_treesitter.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     {
       "nvim-treesitter/nvim-treesitter-context",
-      opts = { max_lines = 5 },
+      opts = { max_lines = 5,  multiwindow = true },
     },
     { "HiPhish/rainbow-delimiters.nvim" },
     { "nvim-treesitter/nvim-treesitter-textobjects" },
