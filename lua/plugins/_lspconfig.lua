19d22d14dc69cc5fd04bba9b22ac3251842e357a
joker <geovannicinto@gmail.com>
2025-04-01 19:32:19 -0600

    feat(lspconfig): add format buffer keymap via conform
    This commit introduces a keymap `<leader>f` to format the current buffer
    using conform.nvim. This enhances the user experience by providing a
    quick and easy way to format code directly from Neovim.

--- lua/plugins/_lspconfig.lua
+++ lua/plugins/_lspconfig.lua
@@ -451,6 +451,23 @@ return {
         },
       },
     },
+    specs = {
+      {
+        "AstroNvim/astrolsp",
+        opts = {
+          mappings = {
+            n = {
+              ["<leader>f"] = {
+                function()
+                  require("conform").format { async = true }
+                end,
+                desc = "Format Buffer",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   {
     "mfussenegger/nvim-lint",
