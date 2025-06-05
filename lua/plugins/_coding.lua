ef244bc654d0a19e8678d07cf3303e47290ec5f0
Gio <geovannicinto@gmail.com>
2025-06-05 07:08:15 -0600

    feat(codecompanion): add support img-clip.nvim

--- lua/plugins/_coding.lua
+++ lua/plugins/_coding.lua
@@ -63,6 +63,18 @@ return {
           },
         },
       },
+      {
+        "HakonHarnes/img-clip.nvim",
+        opts = {
+          filetypes = {
+            codecompanion = {
+              prompt_for_file_name = false,
+              template = "[Image]($FILE_PATH)",
+              use_absolute_path = true,
+            },
+          },
+        },
+      },
     },
   },
   {
