fb1f88d402bd097b53d92cecb544691d90b5164f
Gio <geovannicinto@gmail.com>
2025-11-02 11:19:43 -0600

    refactor(blink): Remove explicit LSP capabilities initialization from blink.cmp

--- lua/plugins/_coding.lua
+++ lua/plugins/_coding.lua
@@ -28,10 +28,6 @@ return {
       sources = { default = { "lsp", "path", "snippets", "buffer" } },
       fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    init = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
-    end,
   },
   {
     "xzbdmw/colorful-menu.nvim",
