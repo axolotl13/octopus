5dadf4a773a10487e4b44a0a6c3f827b7f8674e0
Gio <geovannicinto@gmail.com>
2025-11-06 10:10:51 -0600

    feat(dropbar): add enable config

--- lua/plugins/_ui.lua
+++ lua/plugins/_ui.lua
@@ -295,13 +295,34 @@ return {
     opts = {
       icons = { kinds = { symbols = require("octopus._icons").kind } },
       bar = {
-        attach_events = {
-          "BufEnter",
-          "BufWinEnter",
-          "BufWritePost",
-          "FileType",
-          "LspAttach",
-        },
+        enable = function(buf, win, _)
+          buf = vim._resolve_bufnr(buf)
+          if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+            return false
+          end
+
+          if
+            not vim.api.nvim_buf_is_valid(buf)
+            or not vim.api.nvim_win_is_valid(win)
+            or vim.fn.win_gettype(win) ~= ""
+            or vim.wo[win].winbar ~= ""
+            or vim.bo[buf].ft == "help"
+          then
+            return false
+          end
+
+          local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+          if stat and stat.size > 1024 * 1024 then
+            return false
+          end
+
+          return vim.bo[buf].ft == "markdown"
+            or pcall(vim.treesitter.get_parser, buf)
+            or not vim.tbl_isempty(vim.lsp.get_clients {
+              bufnr = buf,
+              method = "textDocument/documentSymbol",
+            })
+        end,
       },
     },
   },
